1ea276860b3635a849c46eb36d7b6a0832d7168a
orion777-cmd <abiy.biru78@gmail.com>
2024-06-18 21:58:22 +0300

    pass 5th stage

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::collections::HashSet;
 
 fn main() {
     
+    let mut commands: HashSet<&str> = HashSet::new();
+    commands.insert("type");
+    commands.insert("echo");
+    commands.insert("exit");
     // Wait for user input
     loop {
         print!("$ ");
@@ -15,6 +20,14 @@ fn main() {
         match splitted_input.next().unwrap(){
             "exit" => break,
             "echo" => println!("{}", splitted_input.collect::<Vec<&str>>().join(" ")),
+            "type" => {
+                let next_part = splitted_input.next().unwrap();
+                if commands.contains(next_part){
+                    println!("{} is a shell builtin", next_part.trim_end());
+                }else {
+                    println!("{}: not found", next_part.trim_end());
+                }
+            }
             _ => println!("{}: command not found", input.trim_end())
         }
 
